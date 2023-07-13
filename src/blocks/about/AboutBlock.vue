1461ee5709e1b6ab2e5f49925a5f6b7192f183d7
Ksenia Stupina <k.stupina@okenit.ru>
2023-07-13 11:57:30 +0300

    add size to about svg add eslint, vue-router fix vite config

--- src/blocks/about/AboutBlock.vue
+++ src/blocks/about/AboutBlock.vue
@@ -5,16 +5,25 @@
       size="header1"
     />
     <div class="about-content-wrapper">
-      <TypographyText
-        value="Payment as a service, powered seamlessly by our network of shoppers"
-        size="header3"
-      />
+      <div class="about-text-img-wrapper">
+        <TypographyText
+            value="Payment as a service, powered seamlessly by our network of shoppers"
+            size="header3"
+        />
+        <img
+            src="src/assets/two_lines.png"
+            alt="two lines"
+            class="about-img"
+        >
+      </div>
+
       <div class="about-cards-wrapper">
         <div
           v-for="data in aboutData"
           :key="data.title"
           class="about-card-wrapper"
         >
+          <component :is="imageMap[data.imgName]" />
           <TypographyText
             :value="data.title"
             size="header2"
@@ -23,7 +32,7 @@
             :value="data.text"
             size="text"
           />
-          <!--          <component :is="imageMap[data.imgName]" />-->
+
         </div>
       </div>
     </div>
@@ -70,20 +79,31 @@ export default {
   flex-direction: column;
   gap: 20px;
 }
-@media screen and (max-width: 1200px){
-
+.about-text-img-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.about-img {
+  transform: translate(-80px, 80px);
 }
 
 @media screen and (max-width: 900px){
-
+  .about-content-wrapper {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 40px;
+  }
+  .about-img {
+    display: none;
+  }
 }
 
 @media screen and (max-width: 600px){
-  .about-content-wrapper {
-    display: grid;
-    grid-template-columns: 1fr 2fr;
-    gap: 20px;
-  }
-
+.about-cards-wrapper {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 40px;
+}
 }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import svgLoader from 'vite-svg-loader'
+import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    svgLoader(),
+    eslintPlugin()
   ],
   resolve: {
     alias: {
